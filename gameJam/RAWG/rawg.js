7b490a2628d02982e3fe1faa85dbe9b016831b67
Jameson <90803725+jamesonarnett@users.noreply.github.com>
2022-01-03 13:07:55 -0600

    Add blackJack & Jeopardy

--- gameJam/RAWG/rawg.js
+++ gameJam/RAWG/rawg.js
@@ -1,3 +1,21 @@
 // year at bottom
 const year = document.getElementById("year");
 year.textContent = new Date().getFullYear();
+
+fetch(
+  "https://rawg-video-games-database.p.rapidapi.com/games?key=a29976446de649d893a9256fce4e8789",
+  {
+    method: "GET",
+    headers: {
+      "x-rapidapi-host": "rawg-video-games-database.p.rapidapi.com",
+      "x-rapidapi-key": "50c7194bdamshb0eb8a360c84412p1ad793jsn94b395dbc2ad",
+    },
+  }
+)
+  .then((response) => {
+    let data = response.json();
+    console.log(data);
+  })
+  .catch((err) => {
+    console.error(err);
+  });
